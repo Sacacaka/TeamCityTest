57503c025109fa51a2669c934afd5e400ab45d84
Victor Rondeau <victor.rondeau@rail.bombardier.com>
2020-06-01 15:29:43 -0400

    Ajout d'une boucle plutôt qu'un Hello World

--- TeamCityTest/TeamCityTest/Program.cs
+++ TeamCityTest/TeamCityTest/Program.cs
@@ -6,8 +6,10 @@ namespace TeamCityTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Salut le monde!");
-            Console.ReadLine();
+            for (int i = 0; i < 10000; i ++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
