b2ec581d7bd240b83d105018bea53fdca5f0d5dd
Victor Rondeau <victor.rondeau@rail.bombardier.com>
2020-06-01 15:22:32 -0400

    Changement du message

--- TeamCityTest/TeamCityTest/Program.cs
+++ TeamCityTest/TeamCityTest/Program.cs
@@ -6,7 +6,7 @@ namespace TeamCityTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Salut le monde!");
         }
     }
 }
