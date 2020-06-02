368e8ddbbb7f1df205f52a653eba7216647d2642
Victor Rondeau <victor.rondeau@rail.bombardier.com>
2020-06-02 10:57:06 -0400

    Ajout de tests unitaires

---
+++ TeamCityTest/TeamCity/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TeamCity
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine(Sum(2, 3));
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ TeamCityTest/TeamCityUnitTests/UnitTest1.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+
+namespace TeamCityUnitTests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            int result = TeamCity.Program.Sum(2, 3);
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int result = TeamCity.Program.Sum(0, 3);
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            int result = TeamCity.Program.Sum(-2, 3);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
